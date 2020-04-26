60fb0471eaa5495f7885fe921e8a19b12b46d21d
Garth Leedle <g.leedle@students.clark.edu>
2020-04-26 13:06:25 -0700

    3

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-primary bg-primary">
 	<a class="navbar-brand" href="lab8.php">Lab 8 / PHP</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
